76bfd1c1b6879c6ac1052dea497884794b9e96a5
Matheus-Morais <matheus2992morais@gmail.com>
2021-02-26 10:12:48 -0300

    Criado test_carro

---
+++ OO/test_carro.py
@@ -0,0 +1,10 @@
+import unittest
+
+from OO.carro import Motor
+
+
+class CarroTestCase(unittest.TestCase):
+    def teste_velocidade_inicial(self):
+
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
